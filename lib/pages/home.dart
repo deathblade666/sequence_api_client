8f35640f7ec391385ca5068838555d6009538ac4
deathmasia <halo2ofdeath@gmail.com>
2025-07-19 08:05:48 -0400

    move safearea to the views widget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -23,92 +23,92 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        resizeToAvoidBottomInset: true,
-        drawer: Drawer(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Spacer(flex: 1,),
-                  const Text("Rule History"),
-                  Spacer(flex: 1,),
-                  IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: (){
-                      Provider.of<HistoryProvider>(context, listen: false).clearHistory();
-                    },
-                  ),
-                ],
-              ),
-              Expanded(
-                child: Consumer<HistoryProvider>(builder: (context, historyProvider, _) {
-                  final history = historyProvider.items;
-                  if (history.isEmpty) {
-                    return const Center(child: Text("No history yet"));
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      drawer: Drawer(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Spacer(flex: 1,),
+                const Text("Rule History"),
+                Spacer(flex: 1,),
+                IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: (){
+                    Provider.of<HistoryProvider>(context, listen: false).clearHistory();
+                  },
+                ),
+              ],
+            ),
+            Expanded(
+              child: Consumer<HistoryProvider>(builder: (context, historyProvider, _) {
+                final history = historyProvider.items;
+                if (history.isEmpty) {
+                  return const Center(child: Text("No history yet"));
+                }
+                return ListView.builder(
+                  itemCount: history.length,
+                  itemBuilder: (context, index) {
+                    final item = history[index];
+                    return ListTile(
+                      title: Text(item.name),
+                      subtitle: Text(
+                        DateFormat('yyyy-MM-dd hh:mm a')
+                        .format(DateTime.parse(item.timestamp)),
+                      ),
+                    );
                   }
-                  return ListView.builder(
-                    itemCount: history.length,
-                    itemBuilder: (context, index) {
-                      final item = history[index];
-                      return ListTile(
-                        title: Text(item.name),
-                        subtitle: Text(
-                          DateFormat('yyyy-MM-dd hh:mm a')
-                          .format(DateTime.parse(item.timestamp)),
-                        ),
-                      );
-                    }
-                  );
-                }),
-              ),
-            ]
-          )
-        ),
-        appBar: AppBar(
-          title: Text(pageName),
-          leading: DrawerButton(),
-          actions: [
-            if (Platform.isWindows || Platform.isLinux || Platform.isMacOS)
-              IconButton(
-                onPressed: () async {
-                 accountPageKey.currentState?.refreshAccounts();
-                },
-                icon: Icon(Icons.refresh),
-              ),
+                );
+              }),
+            ),
+          ]
+        )
+      ),
+      appBar: AppBar(
+        title: Text(pageName),
+        leading: DrawerButton(),
+        actions: [
+          if (Platform.isWindows || Platform.isLinux || Platform.isMacOS)
+            IconButton(
+              onPressed: () async {
+               accountPageKey.currentState?.refreshAccounts();
+              },
+              icon: Icon(Icons.refresh),
+            ),
+        ],
+      ),
+      body: SafeArea(
+        child: Views(widget.prefs,widget.homepageController, accountPageKey: accountPageKey,)
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            Spacer(flex: 1,),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  widget.homepageController.hasClients ? widget.homepageController.jumpToPage(0) : widget.homepageController.initialPage;
+                  pageName = "Accounts";
+                });
+              },
+              child: Text("Accounts"),
+            ),
+            Spacer(),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  widget.homepageController.hasClients ? widget.homepageController.jumpToPage(1) : widget.homepageController.initialPage;
+                  pageName = "Rules";
+                });
+              },
+              child: Text("Rules"),
+            ),
+            Spacer(),
           ],
         ),
-        body: Views(widget.prefs,widget.homepageController, accountPageKey: accountPageKey,),
-        bottomNavigationBar: BottomAppBar(
-          child: Row(
-            children: [
-              Spacer(flex: 1,),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    widget.homepageController.hasClients ? widget.homepageController.jumpToPage(0) : widget.homepageController.initialPage;
-                    pageName = "Accounts";
-                  });
-                },
-                child: Text("Accounts"),
-              ),
-              Spacer(),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    widget.homepageController.hasClients ? widget.homepageController.jumpToPage(1) : widget.homepageController.initialPage;
-                    pageName = "Rules";
-                  });
-                },
-                child: Text("Rules"),
-              ),
-              Spacer(),
-            ],
-          ),
-        ),
-      )
+      ),
     );
   }
 }
